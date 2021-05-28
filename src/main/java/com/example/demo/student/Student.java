5067fed8bba6d4b9ccd3f623d524192b2bcb1782
daoducnha <daoducnha29493@gmail.com>
2021-05-28 19:39:14 +0700

    first commit

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,13 @@
+package com.example.demo.student;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+    private Integer studentId;
+    private String studentName;
+
+
+}
